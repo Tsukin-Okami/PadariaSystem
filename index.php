291f1c9e5b6087cde587a130582602be1c60f9db
Tsukin-Okami <jhonikauanmb@gmail.com>
2024-11-28 14:48:09 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Padaria</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm bg-light navbar-light justify-content-center">
+        <ul>
+            <li>
+                <a href="#"></a>
+            </li>
+        </ul>
+    </nav>
+</body>
+</html>
